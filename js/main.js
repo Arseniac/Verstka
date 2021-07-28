63cc91d7c089bf9d6347a41aa0ec5e1882d89bbf
Arseniac <88092880+Arseniac@users.noreply.github.com>
2021-07-28 15:43:47 +0300

    Залил сайт #2

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/next.svg" alt="next arrow"></button>',
+    prevArrow: '<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prev arrow"></button>',
+    fade: true,
+
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
+  });
+});
