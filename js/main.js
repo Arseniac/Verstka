8ed870e95ae9c4b926648251af036ca3783fdf12
Arseniac <88092880+Arseniac@users.noreply.github.com>
2021-08-15 03:08:59 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,7 +4,19 @@ $(function(){
         nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-next.svg" alt=""></button>',
         autoplay: true,
         autoplaySpeed: 2000,
-        
+        responsive: [
+            {
+                breakpoint: 1160,
+                settings: {
+                    arrows: false
+                }
+    
+            },
+    
+        ]
     });
     
+    
+
+
 });
